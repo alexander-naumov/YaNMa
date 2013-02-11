fa9e03a06c942fb7867203f8d8cca7b0fc56502b
Alexander Naumov <posix.ru@gmail.com>
2013-02-11 22:32:11 +0100

    change imports and class calls

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-#   Copyright 2011 Alexander Naumov <alexander_naumov@opensuse.org>
+#   Copyright 2012-20132-2013 Alexander Naumov <alexander_naumov@opensuse.org>
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License Version 2 as
@@ -10,41 +10,39 @@
 #   GNU General Public License for more details.
 
 import sys, info
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
-class NetworkSettings(QtGui.QWidget):
+class NetworkSettings(QDialog):
 	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self, parent)
-
-	        self.setGeometry(300, 300, 250, 150)
-	        self.setWindowTitle('Network Settings')
-
-		#self.mainWidget = QtGui.QWidget(self)
-
-		label_interface = QtGui.QLabel(' Interface : '+ info.name, self)
-		label_mac       = QtGui.QLabel(' MAC       : '+ info.HwAddress, self)
-		label_ip        = QtGui.QLabel(' IP        : '+ info.IP4, self)
-		label_driver    = QtGui.QLabel(' Driver    : '+ info.driver, self)
-
-		configure = QtGui.QPushButton('Configure', self)
-                quit      = QtGui.QPushButton('Ok',        self)
+		super(NetworkSettings, self).__init__(parent)
 		
-		self.connect(configure, QtCore.SIGNAL('clicked()'), QtGui.qApp, QtCore.SLOT('quit()'))		
-		self.connect(quit,      QtCore.SIGNAL('clicked()'), QtGui.qApp, QtCore.SLOT('quit()'))
+		self.setGeometry(300, 300, 250, 150)
+		self.setWindowTitle('Network Settings')
+
+		label_interface = QLabel(' Interface : \t'+ info.name, self)
+		label_mac       = QLabel(' MAC       : \t'+ info.HwAddress, self)
+		label_ip        = QLabel(' IP        : \t'+ info.IP4, self)
+		label_driver    = QLabel(' Driver    : \t'+ info.driver, self)
+
+		configure = QPushButton('Configure', self)
+		quit      = QPushButton('Ok',        self)
 		
-		hLayout = QtGui.QHBoxLayout()
+		self.connect(configure, SIGNAL('clicked()'), self.accept)
+		self.connect(quit,      SIGNAL('clicked()'), self.accept)
+		
+		hLayout = QHBoxLayout()
 		hLayout.addWidget(configure)
 		hLayout.addWidget(quit)
 
-		mainLayout = QtGui.QVBoxLayout()
+		mainLayout = QVBoxLayout()
 		mainLayout.addWidget(label_interface)
 		mainLayout.addWidget(label_mac)
-                mainLayout.addWidget(label_ip)
+		mainLayout.addWidget(label_ip)
 		mainLayout.addWidget(label_driver)
 		mainLayout.addLayout(hLayout)
 
 		self.setLayout(mainLayout)
-		
 
 #app = QtGui.QApplication(sys.argv)
 ns = NetworkSettings()
--- yanma_tray.py
+++ yanma_tray.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#   Copyright 2011 Alexander Naumov <alexander_naumov@opensuse.org>
+#   Copyright 2012-2013 Alexander Naumov <alexander_naumov@opensuse.org>
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License Version 2 as
@@ -12,32 +12,31 @@
 #   GNU General Public License for more details.
 
 import sys, info
-from PyQt4 import Qt, QtCore, QtGui
 
-from PyQt4 import *
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
 show_info =  "Interface\t" + info.name + "\n" \
-	     "IP Addr\t" + info.IP4 + "\n" \
-             "MAC Addr\t" + info.HwAddress + "\n" \
-	     "Driver\t" + info.driver
+	     "IP Addr\t"  + info.IP4 + "\n" \
+		 "MAC Addr\t" + info.HwAddress + "\n" \
+	     "Driver\t"   + info.driver
 
 
-class TrayBase(QtGui.QWidget):
-	def __init__(self, *args):
-		apply(QtGui.QWidget.__init__,(self, ) + args)
+class TrayBase(QSystemTrayIcon):
+	def __init__(self, parent=None):
+		super(TrayBase,self).__init__(parent)
 		
-		self.settings = QtCore.QSettings()
+		self.settings = QSettings()
 		self.updateCheck = self.settings.value("checkboxes/update").toBool();
 
-		self.tray = QtGui.QSystemTrayIcon(self)
-		self.trayMenu = QtGui.QMenu()
+		self.tray = QSystemTrayIcon(self)
+		self.trayMenu = QMenu()
 		self.tray.setToolTip(show_info)
 		
-		#self.action_wired = QtGui.QAction(QtGui.QIcon("images/wired.png"), u'Wired Networks', self)
-		self.action_wired = QtGui.QAction(QtGui.QIcon("images/wired.png"), info.name+u' '+info.IP4, self)
-		self.action_wireless = QtGui.QAction(QtGui.QIcon("images/wireless.png"), u'Wireless Networks', self)
-		self.action_settings = QtGui.QAction(QtGui.QIcon("images/settings.png"), u'Network Settings', self)
-		self.action_quit = QtGui.QAction(QtGui.QIcon("images/quit.png"), u'Quit', self)
+		self.action_wired    = QAction(QIcon("images/wired.png"), info.name+u' '+info.IP4, self)
+		self.action_wireless = QAction(QIcon("images/wireless.png"), u'Wireless Networks', self)
+		self.action_settings = QAction(QIcon("images/settings.png"), u'Network Settings', self)
+		self.action_quit     = QAction(QIcon("images/quit.png"), u'Quit', self)
 
 		self.trayMenu.addAction(self.action_wired)
 		self.trayMenu.addSeparator()
@@ -46,12 +45,12 @@ class TrayBase(QtGui.QWidget):
 		self.trayMenu.addAction(self.action_settings)
 		self.trayMenu.addAction(self.action_quit)
 
-		self.connect(self.action_wired, QtCore.SIGNAL("triggered()"), self.wired)
-		self.connect(self.action_wireless, QtCore.SIGNAL("triggered()"), self.wireless)
-		self.connect(self.action_settings, QtCore.SIGNAL("triggered()"), self.net_settings)
-		self.connect(self.action_quit, QtCore.SIGNAL("triggered()"), self.quit)
+		self.connect(self.action_wired, SIGNAL("triggered()"), self.wired)
+		self.connect(self.action_wireless, SIGNAL("triggered()"), self.wireless)
+		self.connect(self.action_settings, SIGNAL("triggered()"), self.net_settings)
+		self.connect(self.action_quit, SIGNAL("triggered()"), self.quit)
 		
-		self.trayIcon = QtGui.QIcon("images/applet_48x48.png")
+		self.trayIcon = QIcon("images/applet_48x48.png")
 		self.tray.setContextMenu(self.trayMenu)
 		self.tray.setIcon(self.trayIcon)
 		
@@ -72,13 +71,13 @@ class TrayBase(QtGui.QWidget):
 		sys.exit()
 
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
+	app = QApplication(sys.argv)
 	
-	if( QtGui.QSystemTrayIcon.isSystemTrayAvailable() != 1 ):
-		message = QtGui.QMessageBox(self)
+	if(QSystemTrayIcon.isSystemTrayAvailable() != 1 ):
+		message = QMessageBox(self)
 		message.setText(u'Could not detect system tray')
-		message.setIcon(QtGui.QMessageBox.Warning)
-		message.addButton("Quit", QtGui.QMessageBox.RejectRole)
+		message.setIcon(QMessageBox.Warning)
+		message.addButton("Quit", QMessageBox.RejectRole)
 		message.exec_()
 		response = message.clickedButton().text()
 		
@@ -87,8 +86,8 @@ if __name__ == "__main__":
 
 	else:
 		print u"YaNMa starting..."
-		settings = QtCore.QSettings()
-		settings.setValue("popupmessages", QtCore.QVariant(QtGui.QSystemTrayIcon.supportsMessages()));
+		settings = QSettings()
+		settings.setValue("popupmessages", QVariant(QSystemTrayIcon.supportsMessages()));
 	
 		app.setQuitOnLastWindowClosed(0)
 		myob = TrayBase()
